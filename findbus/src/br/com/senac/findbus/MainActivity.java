9bd47a1ced13e48a85a8877f5394747de8b54aca
hezrom12 <hezrom12@gmail.com>
2014-09-17 14:09:48 -0300

    alterado icone do app

--- findbus/src/br/com/senac/findbus/MainActivity.java
+++ findbus/src/br/com/senac/findbus/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,8 +15,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	
-
+		
+		final TextView texto2 = (TextView) findViewById(R.id.texto2);
+		Button botao = (Button) findViewById(R.id.botao);
+		
+		botao.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				texto2.setText("Ainda em desenvolvimento");
+			}
+			
+			});
+			
+		
 	}
 
 	@Override
