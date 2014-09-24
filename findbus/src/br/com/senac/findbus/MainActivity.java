eed8e6389226731f3f3a6a17b41500dbaab7af8b
jandrei Carlos Masiero <jandrei@users.noreply.github.com>
2014-09-24 19:19:09 -0300

    ajustes

--- findbus/src/br/com/senac/findbus/MainActivity.java
+++ findbus/src/br/com/senac/findbus/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.senac.findbus;
 
-import br.com.senac.findbus.R;
+import java.util.List;
+
+import br.com.senac.findbus.dao.StopDAO;
+import br.com.senac.findbus.model.StopED;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,22 +15,45 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	StopDAO stopDao;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		stopDao = StopDAO.getInstance(getApplication());
+
 		final TextView texto2 = (TextView) findViewById(R.id.texto2);
 		Button botao = (Button) findViewById(R.id.botao);
-		
-		botao.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v){
+
+		botao.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
 				texto2.setText("Ainda em desenvolvimento");
+
+				try {
+					if (android.os.Build.VERSION.SDK_INT > 9) {
+						StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+								.permitAll().build();
+						StrictMode.setThreadPolicy(policy);
+					}
+
+					String resposta = new SynchronousHttpConnection()
+							.get("http://ec2-54-200-87-69.us-west-2.compute.amazonaws.com:8080/findbusweb/webservice/r/stops");
+
+					stopDao.importarListaJson(resposta);
+					List<StopED> stops = stopDao.listarTodos();
+
+					Mensagens.ExibeMensagemAlert(v.getContext(),
+							"Tamanho da lista = " + stops.size());
+
+				} catch (Exception e) {
+					Mensagens.ExibeExceptionAlert(v.getContext(), e);
+				}
 			}
-			
-			});
-			
-		
+
+		});
+
 	}
 
 	@Override
